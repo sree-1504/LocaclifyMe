d6236c6fb99391a509b7352c0811bd7f4b875f43
sreeja <52134261+sree-1504@users.noreply.github.com>
2020-05-26 08:08:54 +0530

    Add files via upload

---
+++ sreeLM/android/app/src/main/kotlin/com/example/sreeLM/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sreeLM
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ sreeLM/lib/pages/TimeLinePage.dart
@@ -0,0 +1,18 @@
+import 'package:sreeLM/widgets/HeaderWidget.dart';
+import 'package:sreeLM/widgets/ProgressWidget.dart';
+import 'package:flutter/material.dart';
+
+class TimeLinePage extends StatefulWidget {
+  @override
+  _TimeLinePageState createState() => _TimeLinePageState();
+}
+
+class _TimeLinePageState extends State<TimeLinePage> {
+  @override
+  Widget build(context) {
+    return Scaffold(
+      appBar: header(context, isAppTitle:  true),
+      body: circularProgress(),
+    );
+  }
+}
